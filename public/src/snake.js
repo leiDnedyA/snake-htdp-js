f6cfa526f413e2a19059524434e71e112fd917f1
leiDnedyA <aydendiel@gmail.com>
2024-01-13 17:45:38 -0500

    Started writing game

--- public/src/snake.js
+++ public/src/snake.js
@@ -6,12 +6,42 @@ import {
   onKey,
   emptyScene,
   stopWhen,
-  text,
-  overlayXY,
-  aboveAlign,
-  above,
-  besideAlign,
-  beside,
-  placeImage
+  circle,
+  posn,
+  posnX,
+  posnY,
 } from "./js-build/modules/js-wrapper.rkt.js";
+
+
+/*
+ * DATA DEFINITIONS / CONSTRUCTORS
+ * */
+
+function world(snake, food) {
+  return {
+    "snake": snake,
+    "food": food
+  }
+}
+
+function snake(segs, dir) {
+  return {
+    "segs": segs,
+    "dir": dir
+  }
+}
+
+
+/*
+ * CONSTANTS
+ * */
+
+const CELL_SIZE = 10;
+const SNAKE_COLOR = "red";
+const FOOD_COLOR = "green";
+const TICK_PERIOD = .3;
+const GRID_ROWS = 30;
+const GRID_COLS = 30;
+
+const SEG_IMAGE = circle(CELL_SIZE / 2, "solid", SNAKE_COLOR);
 
